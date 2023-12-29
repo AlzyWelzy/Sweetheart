3246795e85fc4a097fbccfa3c40ca692a663336c
AlzyWelzy <welzyalzy@gmail.com>
2023-12-29 20:26:51 +0530

    making the web app

--- app/views.py
+++ app/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from .models import CrushResponse
 
 
 def index(request):
+    # if request.method == "POST":
+    #     name = request.POST.get("name")
+    #     response_value = request.POST.get("response")
+    #     response = response_value == "True"
+    #     CrushResponse.objects.create(name=name, response=response)
+
+    #     if response:
+    #         return render(request, "yes_page.html", {"name": name})
+    #     return render(request, "no_page.html", {"name": name})
+
     return render(request, "index.html")
