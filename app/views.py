ca937b0adb47077548ec0875f6687e8b397bd669
AlzyWelzy <welzyalzy@gmail.com>
2024-01-27 16:21:35 +0530

    build: finishing it up

--- app/views.py
+++ app/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render, redirect
-from .models import CrushResponse
+from .models import Proposal
 
 
 def index(request):
-    # if request.method == "POST":
-    #     name = request.POST.get("name")
-    #     response_value = request.POST.get("response")
-    #     response = response_value == "True"
-    #     CrushResponse.objects.create(name=name, response=response)
+    if request.method == "POST":
+        name = request.POST.get("name")
+        response_value = request.POST.get("response")
+        response = response_value == "True"
+        print(name, response)
+        Proposal.objects.create(name=name, response=response)
 
-    #     if response:
-    #         return render(request, "yes_page.html", {"name": name})
-    #     return render(request, "no_page.html", {"name": name})
+        if response:
+            return render(request, "yes_page.html", {"name": name})
+        else:
+            return render(request, "no_page.html", {"name": name})
 
     return render(request, "index.html")
--- core/settings.py
+++ core/settings.py
@@ -83,14 +83,21 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": config.get("DB_NAME"),
+#         "USER": config.get("DB_USER"),
+#         "PASSWORD": config.get("DB_PASSWORD"),
+#         "HOST": config.get("DB_HOST"),
+#         "PORT": config.get("DB_PORT"),
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config.get("DB_NAME"),
-        "USER": config.get("DB_USER"),
-        "PASSWORD": config.get("DB_PASSWORD"),
-        "HOST": config.get("DB_HOST"),
-        "PORT": config.get("DB_PORT"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
