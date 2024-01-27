f4486e490e967d24ddb126fcf062bd2a240ba52f
AlzyWelzy <welzyalzy@gmail.com>
2024-01-27 22:22:24 +0530

    fixing things in both end

--- core/settings.py
+++ core/settings.py
@@ -144,6 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # STATICFILES_DIRS = [
 #     BASE_DIR / "static",
