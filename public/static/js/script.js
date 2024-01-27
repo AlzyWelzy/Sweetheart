5a510e593f112a4f2031ab392a7ca43313a27829
AlzyWelzy <welzyalzy@gmail.com>
2024-01-27 17:09:59 +0530

    build: final touch ups

--- public/static/js/script.js
+++ public/static/js/script.js
@@ -1,1 +1,15 @@
- 
+function setProposalHeading(username) {
+  document.getElementById("proposalHeading").innerText =
+    username + ", do you want to go out with me?";
+}
+
+function promptName() {
+  let username = prompt("Please enter your name:");
+  document.getElementById("username").value = username;
+  setProposalHeading(username);
+}
+
+formContent = document.getElementById("proposalForm");
+if (formContent != null) {
+  promptName();
+}
