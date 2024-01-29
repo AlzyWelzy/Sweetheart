6311b130f9a5f1b8bb993a0bd4c399e95b05dbda
AlzyWelzy <welzyalzy@gmail.com>
2024-01-29 17:32:07 +0530

    added time stamp

--- app/models.py
+++ app/models.py
@@ -10,9 +10,10 @@ class Proposal(models.Model):
     # )
 
     response = models.CharField(max_length=3)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.name} said {self.response}"
+        return f"{self.name} said {self.response} at {self.timestamp}"
 
 
 admin.site.register(Proposal)
