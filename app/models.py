bd69622f840de1327fd4c24cab20d33597837fec
AlzyWelzy <welzyalzy@gmail.com>
2024-01-29 17:46:55 +0530

    removing old comments

--- app/models.py
+++ app/models.py
@@ -10,7 +10,6 @@ class Proposal(models.Model):
     # )
 
     response = models.CharField(max_length=3)
-    # timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.name} said {self.response}"
