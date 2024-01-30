d1c8dccd1407827b0f3f43ea321de8e96fbe2b71
AlzyWelzy <welzyalzy@gmail.com>
2024-01-30 17:39:38 +0530

    moving files

---
+++ app/utils.py
@@ -0,0 +1,40 @@
+from django.shortcuts import redirect
+from .models import Proposal
+from django.utils import timezone
+import uuid
+
+
+def generate_user_identifier():
+    return str(uuid.uuid4())
+
+
+def create_proposal(name, response, user_identifier):
+    return Proposal.objects.create(
+        name=name,
+        response=response,
+        timestamp=timezone.now(),
+        user_identifier=user_identifier,
+    )
+
+
+def update_proposal_email(proposal, email):
+    proposal.email = email
+    proposal.save()
+
+
+def get_proposal_by_user_identifier(user_identifier):
+    try:
+        return Proposal.objects.get(user_identifier=user_identifier)
+    except Proposal.DoesNotExist:
+        return None
+
+
+def get_proposal_or_redirect(request):
+    user_identifier = request.session.get("user_identifier")
+
+    proposal = get_proposal_by_user_identifier(user_identifier)
+
+    if proposal:
+        return proposal
+    else:
+        return redirect("error")
--- app/views.py
+++ app/views.py
@@ -1,50 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Proposal
-from django.utils import timezone
-import uuid
-
-
-def generate_user_identifier():
-    # Generate a unique user identifier using UUID
-    return str(uuid.uuid4())
-
-
-def create_proposal(name, response, user_identifier):
-    # Create a Proposal instance and save it to the database
-    return Proposal.objects.create(
-        name=name,
-        response=response,
-        timestamp=timezone.now(),
-        user_identifier=user_identifier,
-    )
-
-
-def update_proposal_email(proposal, email):
-    # Update the email field and save the proposal
-    proposal.email = email
-    proposal.save()
-
-
-def get_proposal_by_user_identifier(user_identifier):
-    # Try to get the Proposal associated with the user identifier
-    try:
-        return Proposal.objects.get(user_identifier=user_identifier)
-    except Proposal.DoesNotExist:
-        return None
-
-
-def get_proposal_or_redirect(request):
-    # Retrieve the user identifier from the session
-    user_identifier = request.session.get("user_identifier")
-
-    # Get the Proposal associated with the user identifier
-    proposal = get_proposal_by_user_identifier(user_identifier)
-
-    if proposal:
-        return proposal
-    else:
-        # Handle the case where the Proposal does not exist
-        return redirect("error")
+from .utils import *
 
 
 def index(request):
@@ -52,16 +7,12 @@ def index(request):
         name = request.POST.get("name")
         response = request.POST.get("response")
 
-        # Generate a unique user identifier
         user_identifier = generate_user_identifier()
 
-        # Save the user identifier in the session
         request.session["user_identifier"] = user_identifier
 
-        # Create a Proposal instance and save it to the database
         proposal = create_proposal(name, response, user_identifier)
 
-        # Redirect based on the user's response
         if response == "Yes":
             return redirect("yes_page")
         else:
@@ -78,7 +29,6 @@ def yes_page(request):
     if request.method == "POST":
         email = request.POST.get("email")
 
-        # Update the email field and save the proposal
         update_proposal_email(proposal, email)
 
     return render(request, "yes_page.html", context)
