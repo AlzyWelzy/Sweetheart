052ee593ce48d5183cc2b8f5b53479c2277e536b
AlzyWelzy <welzyalzy@gmail.com>
2024-02-01 21:35:02 +0530

    log

--- core/settings.py
+++ core/settings.py
@@ -157,3 +157,22 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles", "static")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "ERROR",
+            "class": "logging.FileHandler",
+            "filename": "logfile.log",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+    },
+}
